d1187d633316da7020612983a158e6c1f947aed0
yashasvi01 <yashasvi.tiwari01@gmail.com>
2021-04-26 00:11:56 +0530

    Committing chnanges to branch reactjs-demo-1

--- src/App.js
+++ src/App.js
@@ -3,10 +3,51 @@
 import ExpenseItems from "./components/ExpenseItems";
 
 function App() {
+  const expenses = [
+    { id: "it1", title: "TV", amount: 90.34, date: new Date(2020,12,12) },
+    {
+      id: "it2",
+      title: "Car Insurance",
+      amount: 274.34,
+      date: new Date(2021,1,2)
+    },
+    {
+      id: "it3",
+      title: "House Mortgage",
+      amount: 800.12,
+      date: new Date(2022,2,1)
+    },
+    {
+      id: "it4",
+      title: "Wooden Desk",
+      amount: 50.00,
+      date: new Date(2021,4,12)
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItems></ExpenseItems>
+      <ExpenseItems
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItems>
     </div>
   );
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,9 +1,33 @@
-function ExpenseItems() {
+// Demo for props object to pass/access values from one component to another
+
+// Convert date into user readable format using toLocalestring method
+
+import "./ExpenseItems.css";
+
+function ExpenseItems(props) {
+
+  const month = props.date.toLocaleString('en-US',{month:'long'});
+  const year = props.date.toLocaleString('en-US',{year:'numeric'});
+  const day = props.date.toLocaleString('en-US',{day:'numeric'});
+
   return (
-    <div>
-      <div>Date</div>
-      <div>
-        title <div>Amount</div>
+    <div className="expense-item">
+      {/* <div className="date">{props.date.toISOString()}</div> */}
+      {/* Display date in calendar format */}
+
+
+    <div className="date">
+      <div>{month}</div>
+      <div>{year}</div>
+      <div>{day}</div>
+
+    </div>
+
+
+
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
