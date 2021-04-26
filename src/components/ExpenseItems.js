6d4e562e51fd1f380c7e6663d96e83305fb9ffb2
yashasvi01 <yashasvi.tiwari01@gmail.com>
2021-04-27 00:19:59 +0530

    Demo for composition and props.children

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,18 +1,21 @@
 //DEmo for splitting components according to requirement and funtionality to make code look simple and clean
 
+// Demo to use custom components as a wrapper around other content.
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
+import Card from "./Card";
 
 function ExpenseItems(props) {
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       {/* Call the component to extract date   */}
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
--- src/components/ExpensesList.js
+++ src/components/ExpensesList.js
@@ -1,9 +1,10 @@
 import ExpenseItems from "./ExpenseItems";
 import "./ExpensesList.css";
+import Card from "./Card";
 
 function ExpensesList(props) {
   return (
-    <div className="expenses">
+    <Card className="expenses">
       <ExpenseItems
         title={props.items[0].title}
         amount={props.items[0].amount}
@@ -24,7 +25,7 @@ function ExpensesList(props) {
         amount={props.items[3].amount}
         date={props.items[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
