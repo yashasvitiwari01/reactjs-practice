15faefd33784b1d22375dd21f7c03a46419bb7a3
yashasvi01 <yashasvi.tiwari01@gmail.com>
2021-04-26 22:33:58 +0530

    react-demo-3

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,30 +1,13 @@
-// Demo for props object to pass/access values from one component to another
+//DEmo for splitting components according to requirement and funtionality to make code look simple and clean
 
-// Convert date into user readable format using toLocalestring method
-
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 
 function ExpenseItems(props) {
-
-  const month = props.date.toLocaleString('en-US',{month:'long'});
-  const year = props.date.toLocaleString('en-US',{year:'numeric'});
-  const day = props.date.toLocaleString('en-US',{day:'numeric'});
-
   return (
     <div className="expense-item">
-      {/* <div className="date">{props.date.toISOString()}</div> */}
-      {/* Display date in calendar format */}
-
-
-    <div className="date">
-      <div>{month}</div>
-      <div>{year}</div>
-      <div>{day}</div>
-
-    </div>
-
-
-
+      {/* Call the component to extract date   */}
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
