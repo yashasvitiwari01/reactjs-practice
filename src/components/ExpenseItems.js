e7198e79156638252fb39ff123cd94dc745a192b
yashasvi01 <yashasvi.tiwari01@gmail.com>
2021-05-03 08:36:19 +0530

    useState demo

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -2,19 +2,35 @@
 
 // Demo to use custom components as a wrapper around other content.
 
+// Demo for state!!!!!!!!!!!!!!!!1
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItems.css";
 import Card from "./Card";
 
 function ExpenseItems(props) {
+  // a default initial value is passed to useState
+  // the useState also returns something.
+  // the first variable is the current state value and the second element is a function for updating that
+
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle("Updated!!");
+  };
+
+ 
   return (
     <Card className="expense-item">
       {/* Call the component to extract date   */}
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}> Change title</button>
+      
     </Card>
   );
 }
